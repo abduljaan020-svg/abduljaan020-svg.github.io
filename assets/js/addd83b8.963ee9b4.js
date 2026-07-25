695eca20d77702e5dec513eab771239ced301cad
Your Name <your.email@example.com>
2026-07-25 17:23:26 -0400

    Fixed base url

---
+++ assets/js/addd83b8.963ee9b4.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdistro_creation=self.webpackChunkdistro_creation||[]).push([["10"],{1655(t){t.exports=JSON.parse('{"metadata":{"permalink":"/buildadistro.github.io/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
